a26e3c33437412b8a9903ffad8be31bd3ec2ff1e
Arthur Silva <33553579+Incripte@users.noreply.github.com>
2023-07-02 07:27:05 -0300

    1

--- gerador_senha/script.js
+++ gerador_senha/script.js
@@ -1,18 +1,34 @@
 
+const inputEl = document.querySelector("#password")
+
 function generatePassword() {
     const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
 
     let password = ""
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < passwordLength; i++) {
         const randomNumber = Math.floor(Math.random() * chars.length)
         password += chars.substring(randomNumber, randomNumber + 1)
     }
-
-    const inputEl = document.querySelector("#password")
     inputEl.value = password
-
-    console.log(password)
 }
 
+    function copy() {
+        navigator.clipboard.writeText(inputEl.value)
+    }
+
+    let passwordLength = "16"
+
+    const passwordLengthEl = document.querySelector("#password-length")
+    passwordLengthEl.addEventListener("input", function () {
+        passwordLength = passwordLengthEl.value
+
+        generatePassword()
+    })
+
+    const copyButtonEl = document.querySelector("#copy")
+    copyButtonEl.addEventListener("click", copy)
+
 generatePassword()
+
+
