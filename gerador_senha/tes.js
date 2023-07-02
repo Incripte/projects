c8a93f63fdf6136c0bfa25eaffa8eb10445fef46
Arthur Silva <33553579+Incripte@users.noreply.github.com>
2023-07-02 10:49:23 -0300

    10

--- gerador_senha/tes.js
+++ gerador_senha/tes.js
@@ -3,6 +3,9 @@ const inputEl = document.querySelector("#password")
 const upperCaseCheckEl = document.querySelector("#uppercase-check")
 const numberCheckEl = document.querySelector("#number-check")
 const symbolCheckEl = document.querySelector("#symbol-check")
+const securityIndicatorBarEl = document.querySelector(
+  "#security-indicator-bar"
+)
 
 let passwordLength = 16
 
@@ -33,6 +36,64 @@ function generatePassword() {
   }
 
   inputEl.value = password
+
+  calculateQuality()
+  calculateFontSize()
+}
+
+function calculateQuality() {
+  // T*0.25 + M*0.15 + N*0.25 + S*0.35 = 100
+  const percent = Math.round(
+    (passwordLength / 64) * 25 +
+      (upperCaseCheckEl.checked ? 15 : 0) +
+      (numberCheckEl.checked ? 25 : 0) +
+      (symbolCheckEl.checked ? 35 : 0)
+  )
+
+  securityIndicatorBarEl.style.width = `${percent}%`
+
+  if (percent > 69) {
+    // safe
+    securityIndicatorBarEl.classList.remove("critical")
+    securityIndicatorBarEl.classList.remove("warning")
+    securityIndicatorBarEl.classList.add("safe")
+  } else if (percent > 50) {
+    // warning
+    securityIndicatorBarEl.classList.remove("critical")
+    securityIndicatorBarEl.classList.add("warning")
+    securityIndicatorBarEl.classList.remove("safe")
+  } else {
+    // critical
+    securityIndicatorBarEl.classList.add("critical")
+    securityIndicatorBarEl.classList.remove("warning")
+    securityIndicatorBarEl.classList.remove("safe")
+  }
+
+  if (percent >= 100) {
+    securityIndicatorBarEl.classList.add("completed")
+  } else {
+    securityIndicatorBarEl.classList.remove("completed")
+  }
+}
+
+function calculateFontSize() {
+  if (passwordLength > 45) {
+    inputEl.classList.remove("font-sm")
+    inputEl.classList.remove("font-xs")
+    inputEl.classList.add("font-xxs")
+  } else if (passwordLength > 32) {
+    inputEl.classList.remove("font-sm")
+    inputEl.classList.add("font-xs")
+    inputEl.classList.remove("font-xxs")
+  } else if (passwordLength > 22) {
+    inputEl.classList.add("font-sm")
+    inputEl.classList.remove("font-xs")
+    inputEl.classList.remove("font-xxs")
+  } else {
+    inputEl.classList.remove("font-sm")
+    inputEl.classList.remove("font-xs")
+    inputEl.classList.remove("font-xxs")
+  }
 }
 
 function copy() {
@@ -42,6 +103,8 @@ function copy() {
 const passwordLengthEl = document.querySelector("#password-length")
 passwordLengthEl.addEventListener("input", function () {
   passwordLength = passwordLengthEl.value
+  document.querySelector("#password-length-text").innerText =
+    passwordLength
   generatePassword()
 })
 upperCaseCheckEl.addEventListener("click", generatePassword)
