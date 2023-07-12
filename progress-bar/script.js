d5b7b3a566e9e6ea9a23dc27d51d86c7583149fe
Arthur Silva <33553579+Incripte@users.noreply.github.com>
2023-07-12 19:59:29 -0300

    1

---
+++ progress-bar/script.js
@@ -0,0 +1,28 @@
+const progressBar = document.querySelector(".progress")
+const previousBtn = document.querySelector("#previous-btn")
+const nextBtn = document.querySelector("#next-btn")
+const alertEl = document.querySelector("#alert")
+
+let progress = 0;
+
+  function updateProgressBar() {
+    progressBar.style.width = progress + "%"
+  }
+
+  function nextStep() {
+    progress += 10
+    if(progress > 100) progress = 100;
+    updateProgressBar();
+    if(progress >= 100) {
+      alertEl.style.visibility = "visible"
+    }
+  }
+
+  function backStep() {
+    progress -= 10
+    if(progress < 0) progress = 0;
+    updateProgressBar();
+  }
+
+  previousBtn.addEventListener("click",backStep)
+  nextBtn.addEventListener("click", nextStep)
